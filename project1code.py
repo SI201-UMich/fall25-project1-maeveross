36148fcc728ff37fc14e2a3dbd0b29c9b10f91b4
Maeve Ross <maeveross@Maeves-MacBook-Air-2.local>
2025-10-10 00:34:24 -0400

    ran 4 functions to find the max flipper length out of all the species

--- project1code.py
+++ project1code.py
@@ -14,8 +14,7 @@ def load_penguin_data (file1):
     file_content = csv.reader(open_file)
     print (file_content) #iterator that reads each row of your CSV file as a list of strings
     headers = next(file_content) # list of names of columns
-    print (headers)
-
+    # print (headers)
     # get each entry as a list
     if headers[0].strip() == "":
         headers[0] = "id"
@@ -24,7 +23,7 @@ def load_penguin_data (file1):
     for row in file_content:
         if not row:
             continue
-            print (row)
+        # print (row)
         penguin = {}
         for i in range(len(headers)):
             key = headers[i]
@@ -57,8 +56,7 @@ def load_penguin_data (file1):
     open_file.close()
     return penguins
         
-peng = load_penguin_data ('penguins.csv')
-print (peng)
+# print (peng)
 
 def get_flipper_lengths(penguins):
     # Creates a dictionary grouping all flipper lengths by species.
@@ -75,6 +73,51 @@ def get_flipper_lengths(penguins):
             flipper_data[species].append(flipper_length)
 
     return flipper_data
-peng2 = get_flipper_lengths(peng)
-print (peng2)
+# print (peng2)
 
+def find_max_flipper (flipper_data):
+    species_max = {}
+
+    for key, val in flipper_data.items():
+        if val:  # make sure list is not empty
+            species_max[key] = max(val)
+
+    # Find species with the greatest flipper length overall
+    overall_species = ''
+    overall_length = 0
+
+    for species in species_max:
+        flipper_length = species_max[species]
+
+        if flipper_length > overall_length:
+            overall_length = flipper_length
+            overall_species = species
+
+    # Step 3: store result in a dictionary
+    max_species = {
+        "species": overall_species,
+        "max_flipper_length": overall_length
+    }
+
+    return max_species
+def generate_report(max_species):
+
+    species = max_species.get("species")
+    flipper_length = max_species.get("max_flipper_length")
+
+    if species and flipper_length:
+
+        print(f"The species with the greatest flipper length is: {species}")
+        print(f"Maximum flipper length: {flipper_length} mm\n")
+    else:
+        print("No valid flipper length data available.")
+    with open("flipper_report.txt", "w") as f:
+        f.write(f"The species with the greatest flipper length is: {species}\n")
+        f.write(f"Maximum flipper length: {flipper_length} mm\n")
+        print (f)
+
+# calling every function
+penguins = load_penguin_data('penguins.csv')
+flipper_data = get_flipper_lengths(penguins)
+max_species = find_max_flipper(flipper_data)
+generate_report(max_species)
